ef6df54e2e06d399ad3854d65e29316362ca9680
cwardell77 <corey.wardell123@gmail.com>
2016-11-10 20:28:18 -0700

    level3 basic started
    basic Idea for level 3 working

---
+++ Assets/Scripts/GameAndLevelControllers/level3GameController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class level3GameController : BaseLevelController
+{
+    // Use this for initialization
+    void Start()
+    {
+        UnitSpawner.SpawnUnitsInArea(UnitReferences.EnemyFighter1, 20, enemySpawns[0]);
+        UnitSpawner.SpawnUnitsInArea(UnitReferences.EnemyFighter1, 20, enemySpawns[1]);
+        UnitSpawner.SpawnUnitsInArea(UnitReferences.EnemyFighter1, 20, enemySpawns[2]);
+        UnitSpawner.SpawnUnitsInArea(UnitReferences.EnemyCruiser1, 1, enemySpawns[0]);
+        UnitSpawner.SpawnUnitsInArea(UnitReferences.EnemyCruiser1, 1, enemySpawns[1]);
+        UnitSpawner.SpawnUnitsInArea(UnitReferences.AlliedFighter1, 15, allySpawns[0]);
+        UnitSpawner.SpawnUnitsInArea(UnitReferences.AlliedFighter1, 15, allySpawns[1]);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    protected override void PlayerEnteredBoundary(GameObject boundary)
+    {
+        
+    }
+
+
+    protected override void PlayerLeftBoundary(GameObject boundary)
+    {
+
+    }
+}
+
